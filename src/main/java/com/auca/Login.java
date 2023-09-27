c3adb0a09ba71acdad78038524d0da1c8f44a26d
tifan9 <43030276+tifan9@users.noreply.github.com>
2023-09-27 11:45:07 +0200

    fixing the login issue

--- src/main/java/com/auca/Login.java
+++ src/main/java/com/auca/Login.java
@@ -12,11 +12,11 @@ public class Login extends HttpServlet {
 	// Declare Variables
 	private static final String name="admin";
 	private static final String pass="admin";
-	@Override
+	
 	public void service (HttpServletRequest req, HttpServletResponse res) throws IOException{
 		
 		try {
-			String User= req.getParameter("username");
+			String user= req.getParameter("username");
 			String pswd= req.getParameter("password");
 			
 			PrintWriter out = res.getWriter();
@@ -25,7 +25,7 @@ public class Login extends HttpServlet {
 			
 			// check if the username and password matches
 			
-			if(User.equals(name) && pswd.equals(pass)) {
+			if(user.equals(name) && pswd.equals(pass)) {
 				res.sendRedirect("welcome.html");
 				
 			}else {
